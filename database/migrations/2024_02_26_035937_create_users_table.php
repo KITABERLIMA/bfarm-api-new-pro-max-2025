31e9ec0a6f6b2a2fe175fb936ceb7a5e19d52d94
Arima1213 <arima3992@gmail.com>
2024-02-26 11:11:53 +0700

    penambahan migrasi user

--- database/migrations/2024_02_26_035937_create_users_table.php
+++ database/migrations/2024_02_26_035937_create_users_table.php
@@ -13,12 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('role_id');
+            $table->string('user_type', 255);
+            $table->string('subs_status', 255);
+            $table->string('token', 255);
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('role');
         });
     }
 
