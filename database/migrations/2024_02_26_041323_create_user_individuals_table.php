f1d0d91f3c244b5cee5383691815fc6c29d1645c
Arima1213 <arima3992@gmail.com>
2024-02-26 11:13:49 +0700

    penambahan migrasi uer individual

---
+++ database/migrations/2024_02_26_041323_create_user_individuals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_individuals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('address_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
+            $table->string('phone', 20);
+            $table->timestamps();
+
+            $table->foreign('address_id')->references('id')->on('address');
+            $table->foreign('user_id')->references('id')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_individuals');
+    }
+};
