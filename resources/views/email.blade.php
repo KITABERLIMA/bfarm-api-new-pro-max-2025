be97331963c28aeae41c913ac95bc84288adb643
Arima1213 <arima3992@gmail.com>
2024-04-24 13:50:29 +0700

    nnn

---
+++ resources/views/email.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>OTP Code</title>
+</head>
+<body>
+    <p>Dear User,</p>
+    <p>You have requested an OTP (One-Time Password) code. Please use the following code to proceed with your authentication process:</p>
+    <h2>{{ $otp }}</h2>
+    <p>This OTP is valid for the next 10 minutes. Please do not share this OTP with anyone.</p>
+    <p>If you did not request this OTP, please ignore this email or contact our support team.</p>
+    <p>Thank you!</p>
+</body>
+</html>
