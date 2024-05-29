659079e18872444f91b24a8c1b0fedf96781893b
Arima1213 <arima3992@gmail.com>
2024-05-29 14:38:53 +0700

    chore: Refactor user_individuals migration and add required fields

--- database/migrations/2024_02_26_041323_create_user_individuals_table.php
+++ database/migrations/2024_02_26_041323_create_user_individuals_table.php
@@ -6,30 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('user_individuals', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('address_id')->required();
-            $table->unsignedBigInteger('user_id')->required();
-            $table->string('first_name', 255)->required();
-            $table->string('last_name', 255)->required();
-            $table->string('phone', 20)->required();
-            $table->timestamps();
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('user_individuals', function (Blueprint $table) {
+      $table->id();
+      $table->unsignedBigInteger('address_id')->required();
+      $table->unsignedBigInteger('user_id')->required();
+      $table->string('first_name', 255)->required();
+      $table->string('last_name', 255)->required();
+      $table->string('phone', 20)->required();
+      $table->timestamps();
 
-            // $table->foreign('address_id')->references('id')->on('address');
-            // $table->foreign('user_id')->references('id')->on('user');
-        });
-    }
+      // $table->foreign('address_id')->references('id')->on('address');
+      // $table->foreign('user_id')->references('id')->on('user');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('user_individuals');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('user_individuals');
+  }
 };
