c9299544b0b49e0eba487e0866d46f931ba969db
Arima1213 <arima3992@gmail.com>
2024-05-29 14:39:28 +0700

    chore: Refactor user_individuals model and update relationships

--- app/Models/User_individual.php
+++ app/Models/User_individual.php
@@ -9,28 +9,28 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class user_individual extends Model
 {
-    use HasFactory;
-    protected $table = 'user_individuals';
-    protected $primaryKey = 'id';
-    public $incrementing = true;
-    protected $keyType = 'int';
-    public $timestamps = true;
+  use HasFactory;
+  protected $table = 'user_individuals';
+  protected $primaryKey = 'id';
+  public $incrementing = true;
+  protected $keyType = 'int';
+  public $timestamps = true;
 
-    protected $fillable = [
-        'user_id',
-        'address_id',
-        'first_name',
-        'last_name',
-        'phone',
-    ];
+  protected $fillable = [
+    'user_id',
+    'address_id',
+    'first_name',
+    'last_name',
+    'phone',
+  ];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
+  public function user()
+  {
+    return $this->belongsTo(User::class, 'user_id');
+  }
 
-    public function address()
-    {
-        return $this->belongsTo(Address::class, 'address_id');
-    }
+  public function address()
+  {
+    return $this->belongsTo(Address::class, 'address_id');
+  }
 }
