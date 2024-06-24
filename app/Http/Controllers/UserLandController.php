5c6eb3c2c4e9965d3714b9f097ace5fc687c0b7c
Arima1213 <arima3992@gmail.com>
2024-06-24 11:52:35 +0700

    chore: Refactor UserLandController to remove unused store method and related code

--- app/Http/Controllers/UserLandController.php
+++ app/Http/Controllers/UserLandController.php
@@ -25,49 +25,4 @@ class UserLandController extends Controller
       'data' => $lands
     ]);
   }
-
-
-  public function store(StoreLandRequest $request): JsonResponse
-  {
-    $validated = $request->validated();
-
-    // Mengambil user_id dari pengguna yang saat ini login
-    $userId = auth()->user()->id;
-
-    // Membuat alamat terlebih dahulu dari data yang divalidasi
-    $addressData = [
-      'full_address' => $validated['full_address'],
-      'village' => $validated['village'],
-      'sub_district' => $validated['sub_district'],
-      'city_district' => $validated['city_district'],
-      'province' => $validated['province'],
-      'postal_code' => $validated['postal_code'],
-    ];
-    $address = Address::create($addressData);
-
-    // Setelah alamat berhasil dibuat, buat lahan dengan mengaitkan address_id dan user_id dari pengguna yang login
-    $landData = [
-      'user_id' => $userId,
-      'address_id' => $address->id,
-      'land_status' => $validated['land_status'] ?? null,
-      'land_description' => $validated['land_description'] ?? null,
-      'ownership_status' => $validated['ownership_status'] ?? null,
-      'location' => $validated['location'],
-      'land_area' => $validated['land_area'],
-    ];
-    $land = Land::create($landData);
-
-    if ($land) {
-      return response()->json([
-        'success' => true,
-        'message' => 'Land successfully created',
-        'data' => $land
-      ]);
-    } else {
-      return response()->json([
-        'success' => false,
-        'message' => 'Failed to create land',
-      ], 500);
-    }
-  }
 }
